baa68861c7ae351bda3f59576119cf94a494145e
Danxark <dancaralonzo@gmail.com>
2024-09-21 22:17:53 -0500

    done

---
+++ scripts/getDates.js
@@ -0,0 +1,15 @@
+let lastModified = document.querySelector(".lastModified")
+let currentYear = document.querySelector("#currentyear")
+const lastModifiedOptions = {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+};
+const currentYearOptions = {
+    year: "numeric",
+}
+lastModified.textContent = ` Last Modified: ${new Date(document.lastModified).toLocaleDateString("en-US", lastModifiedOptions)}`;
+currentYear.textContent = `${new Date().toLocaleDateString("en-US", currentYearOptions)}`
