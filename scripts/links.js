29dc41f342d574f5cbda04f09d60728811261b49
Danxark <dancaralonzo@gmail.com>
2024-10-18 10:30:27 -0400

    done

---
+++ scripts/links.js
@@ -0,0 +1,34 @@
+const baseURL = "https://evelynjimena.github.io/wdd230/";
+const linksURL = "https://evelynjimena.github.io/wdd230/data/links.json";
+
+async function getLinks(linksURL) {
+  try {
+    const response = await fetch(linksURL);
+    if (!response.ok) {
+      throw new Error('Failed to fetch links data');
+    }
+    const data = await response.json();
+    const { weeks } = data; 
+    displayLinks(weeks);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getLinks(linksURL);
+
+const weeksList = document.querySelector("#headerThree + ul");
+
+function displayLinks(weeks) {
+  weeks.forEach((weekData) => {
+    const { week, links } = weekData;
+    const listItem = document.createElement("li");
+    const listItemContent = document.createElement("a");
+    const linksHTML = links
+      .map((link) => `<a href="${link.url}">${link.title}</a>`)
+      .join(" | ");
+    listItemContent.innerHTML = `${week}: ${linksHTML}`;
+    listItem.appendChild(listItemContent);
+    weeksList.appendChild(listItem);
+  });
+}
