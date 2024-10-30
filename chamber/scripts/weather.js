c178730fb56d12531105ff9adbc3457c81aa246f
Danxark <dancaralonzo@gmail.com>
2024-10-29 23:23:11 -0500

    done

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-1.02&lon=-79.46&units=imperial&appid=ba8d6824cfeb4c1d90136d5c049a1d34"
+
+async function apiFetch(url) {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+      } else {
+        throw new Error('API request failed');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+  }
+apiFetch(url);
